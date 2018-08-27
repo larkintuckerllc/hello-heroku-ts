789010f679d413daaec7e95aac4d28fe0394db9f
John Tucker <john.tucker@sharpspring.com>
2018-08-27 08:10:35 -0400

    Heroko config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 
+const PORT = process.env.PORT || '5000';
 const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
