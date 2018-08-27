4d8e484d0b2d8335a20cc7918efd708c81e47862
John Tucker <john.tucker@sharpspring.com>
2018-08-27 18:43:27 -0400

    Updated to Test Pipeline

--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import express from 'express';
 
 const PORT = process.env.PORT || '5000';
 const app = express();
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World 2!'));
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
